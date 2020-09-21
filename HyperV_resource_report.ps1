08b139e043d1153ff5cf60ba41e79489e71a5bb0
VladimirKosyuk <42845051+VladimirKosyuk@users.noreply.github.com>
2020-09-21 19:09:17 +0300

    Add files via upload

---
+++ HyperV_resource_report.ps1
@@ -0,0 +1,18 @@
+﻿#Created by https://github.com/VladimirKosyuk
+
+#Outputs as sum VHD, RAM, CPU for all of all VM that stored on HyperV cluster. I know about Get-HyperVReport, but i want to simplify this task
+#
+# Build date: 21.09.2020									   
+ 
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+$Store_size = Get-VM -ComputerName (Get-ClusterNode) | where {($_.State -eq 'Running') -and ($_.Name -notlike '*test*')} | ForEach-Object {
+Get-VHD -ComputerName $_.ComputerName -VMId $_.VMId} | Select -Property path,computername,vhdtype,@{label='Size(GB)';expression={$_.filesize/1gb -as [int]}}
+$Ram_size = Get-VM -ComputerName (Get-ClusterNode) | where {($_.State -eq 'Running') -and ($_.Name -notlike '*test*')} | Select-Object VMName, @{label='Size(GB)';expression={$_.MemoryAssigned/1gb -as [int]}}
+$CPU_size = Get-VM -ComputerName (Get-ClusterNode) | where {($_.State -eq 'Running') -and ($_.Name -notlike '*test*')} | Select-Object VMName, ProcessorCount
+
+Write-output ("VHD "+($Store_size | Select-Object -ExpandProperty 'Size(GB)' | measure -sum | Select-Object -ExpandProperty 'SUM')+" GB")
+Write-output ("RAM "+($Ram_size | Select-Object -ExpandProperty 'Size(GB)' | measure -sum | Select-Object -ExpandProperty 'SUM')+" GB")
+Write-output ("CPU "+($CPU_size | Select-Object -ExpandProperty 'ProcessorCount' | measure -sum | Select-Object -ExpandProperty 'SUM')+" GB")
+
+Remove-Variable -Name * -Force -ErrorAction SilentlyContinue
